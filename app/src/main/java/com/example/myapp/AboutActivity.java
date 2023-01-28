a0aa29b728dd82dcedc70dd1bd02d95c330f5455
tomasprikasky <tom.prikasky@seznam.cz>
2023-01-28 19:15:35 +0100

    o aplikaci/ About aplication

--- app/src/main/java/com/example/myapp/AboutActivity.java
+++ app/src/main/java/com/example/myapp/AboutActivity.java
@@ -1,14 +1,19 @@
 package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+public class AboutActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ImageButton home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
+
     }
 }

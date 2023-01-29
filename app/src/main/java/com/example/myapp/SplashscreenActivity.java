540e8e892d2b44839acaa6ca6eee5413b3a51600
tomasprikasky <tom.prikasky@seznam.cz>
2023-01-29 09:29:06 +0100

    přidání splashscreenactivity + vytvoření ikony

---
+++ app/src/main/java/com/example/myapp/SplashscreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashscreenActivity.this, AboutActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+
+
+}
